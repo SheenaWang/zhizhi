55454953be3d48aa6832a101d514b797473e3579
刘述清 <liushuqing@weidian.com>
2016-05-25 18:59:12 +0800

    框架

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,12 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/hello', function(req, res, next) {
+  res.render('hello', { title: 'HELLO',view:'new umbre' });
+});
+router.get('/tab/:number', function(req, res, next) {
+  var number=req.params.number;
+  var name='哈哈哈哈'+number;
+  res.render('tab', { title: 'HELLO',view:'new umbre' ,name:name});
+});
 module.exports = router;
